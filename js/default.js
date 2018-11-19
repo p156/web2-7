a29d3ae1e06c00d51d7222e6bf123aecc590f4a3
p156 <38420559+p156@users.noreply.github.com>
2018-11-19 18:10:08 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,4 +2,8 @@ window.alert('こんにちは世界！');
 console.log('こんにちは世界！');
 document.getElementById('choice').textContent = new Date();
 var result = window.confirm('準備ok?');
-document.getElementById('choice').textContent = result;
+if(result){
+  document.getElementById('choice').textContent = 'okが押されました';
+}else{
+  document.getElementById('choice').textContent = 'キャンセルが押されました';
+}
