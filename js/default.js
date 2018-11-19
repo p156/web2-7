b6ce1523aaf5a1273721763109fc1c33c4890360
p156 <38420559+p156@users.noreply.github.com>
2018-11-19 17:33:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
 window.alert('こんにちは世界！');
 console.log('こんにちは世界！');
 document.getElementById('choice').textContent = new Date();
+var result = window.confirm('準備ok?');
+document.getElementById('choice').textContent = result;
